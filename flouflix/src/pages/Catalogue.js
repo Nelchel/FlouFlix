441609137e3d1268100963d19861877c557d0dd8
Aurore Wentzel <aurore.wentzel@infomaniak.com>
2022-03-15 15:38:04 +0100

    fix link

--- flouflix/src/pages/Catalogue.js
+++ flouflix/src/pages/Catalogue.js
@@ -10,8 +10,9 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { doc, updateDoc, getDoc } from "firebase/firestore";
-import StarIcon from '@mui/icons-material/Star';
+import StarIcon from "@mui/icons-material/Star";
 import { BrowserRouter as Router, Link, Outlet } from "react-router-dom";
+import { Button } from "@mui/material";
 
 const makeClass = makeStyles((theme) => ({
   signupButton: {
@@ -28,68 +29,68 @@ function Catalogue() {
   const [userRef, setRefUser] = useState("");
   const [userData, setUserData] = useState([]);
   const [upFavoris, setUpFavoris] = useState([]);
-  const [downFavoris, setDownFavoris] = useState('');
+  const [downFavoris, setDownFavoris] = useState("");
 
   useEffect(() => {
     let movies = [];
     db.collection("movies")
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        movies.push(doc.data());
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          movies.push(doc.data());
+        });
+        setMovies(movies);
       });
-      setMovies(movies);
-    });
-  },[]) 
+  }, []);
 
-  useEffect( () => {
+  useEffect(() => {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
         setUidUser(user.uid);
       }
     });
-  },[])
+  }, []);
 
   useEffect(async () => {
     setRefUser(await doc(db, "users", uidUser));
     setUserData(await (await getDoc(userRef)).data());
-  },[uidUser, getMovies, upFavoris, downFavoris])
+  }, [uidUser, getMovies, upFavoris, downFavoris]);
 
-  useEffect( async () => {
-    if(userRef && userData && upFavoris.length > 0){
-        await updateDoc(userRef, {
-          favoris: userData.favoris ? userData.favoris.concat(upFavoris) : upFavoris,
-        });
-      setUpFavoris([])
-    }
-    else console.log("Stop")
-    console.log(uidUser)
-  },[upFavoris])
+  useEffect(async () => {
+    if (userRef && userData && upFavoris.length > 0) {
+      await updateDoc(userRef, {
+        favoris: userData.favoris
+          ? userData.favoris.concat(upFavoris)
+          : upFavoris,
+      });
+      setUpFavoris([]);
+    } else console.log("Stop");
+    console.log(uidUser);
+  }, [upFavoris]);
 
-  useEffect( async () => {
-    console.log(downFavoris)
-    if(downFavoris){
-        await updateDoc(userRef, {
-          favoris: userData.favoris.filter(e => e !== downFavoris),
-        });
-        setDownFavoris('')
-    }
-    else console.log("Stop")
-    console.log(uidUser)
-  }, [downFavoris])
+  useEffect(async () => {
+    console.log(downFavoris);
+    if (downFavoris) {
+      await updateDoc(userRef, {
+        favoris: userData.favoris.filter((e) => e !== downFavoris),
+      });
+      setDownFavoris("");
+    } else console.log("Stop");
+    console.log(uidUser);
+  }, [downFavoris]);
 
   const handleSubmit = (movieId, method) => {
-    console.log(method)
-    if (!method) setUpFavoris([movieId])
+    console.log(method);
+    if (!method) setUpFavoris([movieId]);
     else {
-      setDownFavoris(movieId)
-      console.log(downFavoris)
+      setDownFavoris(movieId);
+      console.log(downFavoris);
     }
-  }
+  };
 
   const isFavoris = (movieId) => {
-    return (userData?.favoris?.includes(movieId));
-  }
+    return userData?.favoris?.includes(movieId);
+  };
 
   return (
     <Box>
@@ -97,28 +98,33 @@ function Catalogue() {
       <Box display="flex" justifyContent="space-evenly">
         {getMovies.map((movie, index) => {
           return (
-
-            <Link to={'/movie/' + movie.id}>
-              <Box maxWidth="345px">
-                <Card style={{ minWidth: "345px" }}>
-                <StarIcon style={isFavoris(movie.id) ? {fill:"red"} : {fill:"grey"}} onClick={() => handleSubmit(movie.id, isFavoris(movie.id))}/>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={movie.url}
-                    alt="green iguana"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {movie.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {movie.description}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Box>
-            </Link>
+            <Box maxWidth="345px">
+              <Card style={{ minWidth: "345px" }}>
+                <StarIcon
+                  style={
+                    isFavoris(movie.id) ? { fill: "red" } : { fill: "grey" }
+                  }
+                  onClick={() => handleSubmit(movie.id, isFavoris(movie.id))}
+                />
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={movie.url}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {movie.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {movie.description}
+                  </Typography>
+                  <Link to={"/movie/" + movie.id}>
+                    <Button>Voir la fiche</Button>
+                  </Link>
+                </CardContent>
+              </Card>
+            </Box>
           );
         })}
       </Box>
