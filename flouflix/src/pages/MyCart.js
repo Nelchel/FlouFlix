ddd1ee0c95ae16918675c774ac6f59b5d3cfe2d5
Souren <galloudek.rio@gmail.Com>
2022-03-16 17:18:35 +0100

    Modification de la quantité, + debounce

--- flouflix/src/pages/MyCart.js
+++ flouflix/src/pages/MyCart.js
@@ -7,10 +7,12 @@ import TextField from "@mui/material/TextField";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import Modal from "@mui/material/Modal";
 import React, { useState, useEffect } from "react";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+// import { useDebouncedCallback } from 'use-debounce';
 import {
   getAuth,
   signOut,
@@ -28,48 +30,34 @@ import {
   arrayRemove,
 } from "firebase/firestore";
 
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 function MyCart() {
   const db = firebase.firestore();
   const auth = getAuth();
-
   const [uid, setUid] = useState("");
   const [myCart, setMyCart] = useState([]);
   const [moovieInMyCart, setMoovieInMyCart] = useState([]);
+  const [quantite, setQuantite] = useState(1);
   const [userCurrent, setUserCurrent] = useState(undefined);
   const moviesCurrent = useState();
   const [newUser, setNewUser] = useState();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const getMyCart = [];
   const getMoovieInMyCart = [];
-
-  // const test = async () => {
-  //   if (uid !== "") {
-  //     const q = query(collection(db, "users"), where("uid", "==", uid));
-  //     const querySnapshot = await getDocs(q);
-
-  //     querySnapshot.forEach((doc) => {
-  //       user.push(doc.data());
-  //     });
-  //     userCurrent[0] = user;
-  //   }
-  //   console.log(userCurrent[0][0].myCart);
-
-  //   const test = userCurrent[0][0].myCart;
-
-  //   if (userCurrent[0][0] !== undefined) {
-  //     userCurrent[0][0].myCart.forEach(async (cart, index) => {
-  //       const s = query(collection(db, "movies"), where("id", "==", cart));
-  //       const querySnapshot = await getDocs(s);
-
-  //       querySnapshot.forEach((doc) => {
-  //         movies.push(doc.data());
-  //         console.log(doc.data())
-  //       });
-  //       console.log(movies[0].releaseDate)
-  //       moviesCurrent.push(movies[0]);
-  //       setMoovieInMyCart(movies);
-  //     })
-  //   }
-  // }
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(async () => {
@@ -97,7 +85,6 @@ function MyCart() {
   }, [uid]);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-
   useEffect(async () => {
     let movies = [];
     if (userCurrent !== undefined) {
@@ -122,6 +109,28 @@ function MyCart() {
       });
       window.location.replace("/mon-panier");
   }
+  
+//   const debounce = useDebouncedCallback(() =>console.log('mise a jour de la base de donnée'),3000)
+// //  {
+// //       const handleClick = async(index) => {
+// //       await updateDoc(doc(db, "users", uid),{
+// //         myCart:arrayRemove({idMoovie :moovieInMyCart[index].id,Quantity : 1})
+// //       });
+// //       window.location.replace("/mon-panier");
+// //   }
+// // }
+// // ,3000)
+  
+  const handleChange = async(e,index) => {
+    if(e.target.value>=0 && e.target.value <= 100)
+    {
+      setQuantite(e.target.value)
+      // debounce()
+    }
+  }
+  
+
+
   return (
     <section>
       <Box>
@@ -141,7 +150,32 @@ function MyCart() {
                     <Typography gutterBottom variant="h5" component="div">
                       {cart.name}
                     </Typography>
-                    <Button onClick={() =>handleClick(index)}>Supprimer</Button>
+                    <Button onClick={handleOpen}>Supprimer</Button>
+                    <Modal
+                      open={open}
+                      onClose={handleClose}
+                      aria-labelledby="modal-modal-title"
+                      aria-describedby="modal-modal-description"
+                    >
+                      <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                          Supprimer le film {cart.name}
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                          Êtes-vous sûr ?
+                        </Typography>
+                        <Button onClick={() =>handleClick(index)} color="error">
+                          Supprimer
+                        </Button>
+                      </Box>
+                  </Modal>
+                  <TextField
+                    value={quantite}
+                    id="outlined-required"
+                    label="Qté"
+                    type="number"
+                    onChange={(e)=> handleChange(e,index)}
+                  />
                   </CardContent>
                 </Card>
               </Box>
