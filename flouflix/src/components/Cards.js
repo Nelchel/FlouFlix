6e2e5c89036428b8c118870db54e618a57133907
Aurore Wentzel <aurore.wentzel@infomaniak.com>
2022-04-27 22:23:41 +0200

    manage some style to cards

--- flouflix/src/components/Cards.js
+++ flouflix/src/components/Cards.js
@@ -12,6 +12,48 @@ import { Button } from "@mui/material";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import Carousel from "react-multi-carousel";
+import { makeStyles } from "@mui/styles";
+
+const makeClass = makeStyles((theme) => ({
+  cardContainer: {
+    backgroundColor: `${theme.palette.primary.main} !important`,
+    minWidth: "345px",
+    color: theme.palette.text.white,
+  },
+  linkToFilm: {
+    color: theme.palette.text.white,
+    textDecoration: "unset",
+    textTransform: "initial",
+    "&:hover": {
+      textDecoration: "underline",
+    },
+  },
+  link: {
+    color: theme.palette.text.white,
+    textDecoration: "unset",
+  },
+  cardContent: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingRight: "0 !important",
+    paddingLeft: "0 !important",
+  },
+  starFavoris: {
+    position: "absolute",
+    right: "10px",
+    top: "10px",
+    cursor: "pointer",
+  },
+  titleCategory: {
+    marginLeft: "20px !important",
+    padding: "25px 0 15px 0",
+  },
+  cardContentContainer: {
+    backgroundColor: theme.palette.primary.dark,
+    padding: "10px !important",
+  },
+}));
 
 function Cards(props) {
   const { title, movies, isCardFavori } = props;
@@ -24,6 +66,7 @@ function Cards(props) {
     }
   };
 
+  const classes = makeClass();
   const handleDragStart = (e) => e.preventDefault();
 
   const isFavoris = (movieId) => {
@@ -57,34 +100,46 @@ function Cards(props) {
   // console.log(movie)
   return (
     <>
-      <Typography variant="h5">{title}</Typography>
+      <Typography variant="h5" className={classes.titleCategory}>
+        {title}
+      </Typography>
       <Carousel responsive={responsive}>
         {movies.map((movie, index) => {
           return (
-            <Box maxWidth="345px" marginLeft="20px">
-              <Card style={{ minWidth: "345px" }}>
+            <Box maxWidth="345px" marginLeft="20px" position="relative">
+              <Card className={classes.cardContainer} elevation={0}>
                 <StarIcon
+                  className={classes.starFavoris}
                   style={
-                    isFavoris(movie.id) ? { fill: "red" } : { fill: "grey" }
+                    isFavoris(movie.id)
+                      ? { fill: "yellow" }
+                      : { fill: "#282828" }
                   }
                   onClick={() => handleSubmit(movie.id)}
                 />
                 <CardMedia
                   component="img"
-                  height="140"
+                  height="360"
                   image={movie.url}
-                  alt="green iguana"
+                  alt={movie.name}
                 />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {movie.name}
+                <CardContent className={classes.cardContentContainer}>
+                  <div className={classes.cardContent}>
+                    <Typography color="white" variant="h5">
+                      {movie.name}
+                    </Typography>
+                    <Link to={"/movie/" + movie.id} className={classes.link}>
+                      <Typography
+                        variant="body1"
+                        className={classes.linkToFilm}
+                      >
+                        Plus d'infos →
+                      </Typography>
+                    </Link>
+                  </div>
+                  <Typography variant="body2" color="gray">
+                    {movie.releaseDate}
                   </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {movie.description}
-                  </Typography>
-                  <Link to={"/movie/" + movie.id}>
-                    <Button>Voir la fiche</Button>
-                  </Link>
                 </CardContent>
               </Card>
             </Box>
@@ -93,24 +148,6 @@ function Cards(props) {
       </Carousel>
     </>
   );
-
-  // return (
-  //   (
-  //     <>
-  //       {(!isCardFavori || (props.userData?.favoris?.length > 0)) && <Typography variant="h5">{title}</Typography>}
-  //       <Carousel responsive={responsive}>
-  //         {movies.map((movie, index) => {
-  //           console.log(movies)
-  //           // if (isCardFavori) {
-  //           //   if (props.userData?.favoris?.includes(movie.id))
-  //           //     return createCard(movie);
-  //           // } else return createCard(movie);
-  //           return createCard(movie)
-  //         })}
-  //       </Carousel>
-  //     </>
-  //   ) || <></>
-  // );
 }
 
 export default Cards;
