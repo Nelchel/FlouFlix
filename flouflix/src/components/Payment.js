8537f89a9d544912b682d3d644dde1d3b6baf3f2
Souren <galloudek.rio@gmail.Com>
2022-05-30 14:34:03 +0200

    hotfix 4

--- flouflix/src/components/Payment.js
+++ flouflix/src/components/Payment.js
@@ -1,27 +1,53 @@
-import React, { useEeffect, useState } from 'react';
-import { Elements, StripeProvider,CardElement, injectStripe }    from 'react-stripe-elements';
-import firebase from "firebase/compat/app";
-import {PaymentElement} from '@stripe/react-stripe-js'
+import React from 'react';
+import {PaymentElement,CardElement,CardExpiryElement,CardNumberElement,CardCvcElement,Elements,useStripe, useElements} from '@stripe/react-stripe-js'
 import {loadStripe} from '@stripe/stripe-js';
 import Button from "@mui/material/Button";
 
 const Payment = (stripeConfig) => {
-
-    const [customer, setCustomer]  = useState(null);
-    const [payment, setPayment]    = useState(false);
-
     const StripePromise = loadStripe("pk_test_51L526RGH7Y6DbZsDauEw1anemg27mScrSuK7a3WOzhDx08m0vjZuyvytTzXMKyXCHQT53pw60DdQOF4aOeEnJ7To00HVayNsSM")
 
-        
-    return  (
-        <section>
-            <Elements>
-                <PaymentElement/>
-            </Elements>
-        </section>
 
+
+    const stripe = useStripe();
+    const elements = useElements();
+  
+    const handleSubmit = async (event) => {
+      // We don't want to let default form submission happen here,
+      // which would refresh the page.
+      event.preventDefault();
+  
+      if (!stripe || !elements) {
+        // Stripe.js has not yet loaded.
+        // Make sure to disable form submission until Stripe.js has loaded.
+        return;
+      }
+  
+      const result = await stripe.confirmPayment({
+        //`Elements` instance that was used to create the Payment Element
+        elements,
+        confirmParams: {
+          return_url: "https://example.com/order/123/complete",
+        },
+      });
+  
+      if (result.error) {
+        // Show error to your customer (for example, payment details incomplete)
+        console.log(result.error.message);
+      } else {
+          console.log("Payment sécurisé")
+        // Your customer will be redirected to your `return_url`. For some payment
+        // methods like iDEAL, your customer will be redirected to an intermediate
+        // site first to authorize the payment, then redirected to the `return_url`.
+      }
+    };
+
+
+    return  (
+        <Elements Stripe={StripePromise}>
+            <CardElement/>
+            <Button disabled={!stripe}>Submit</Button>
+        </Elements>
     );
-    
 }
 
 export default Payment;
