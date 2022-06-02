d8c1c4c0234a57ed29bccb67facd163f30715685
Aurore Wentzel <aurore.wentzel@infomaniak.com>
2022-06-02 16:23:09 +0200

    add notifications system

--- flouflix/src/components/NotificationsBoard.js
+++ flouflix/src/components/NotificationsBoard.js
@@ -1,10 +1,10 @@
 import { Modal, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
-import { makeStyles } from "@mui/styles";
+import { makeStyles, useTheme } from "@mui/styles";
 import Button from "@mui/material/Button";
-
-import React from "react";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import firebase from "firebase/compat/app";
 
 const makeClass = makeStyles((theme) => ({
   signOut: {
@@ -27,11 +27,85 @@ const style = {
 function NotificationsBoard({ open, handleClose }) {
   const classes = makeClass();
   const auth = getAuth();
+  const db = firebase.firestore();
+  const theme = useTheme();
+
+  const [uid, setUid] = useState("");
+  const getNotifications = [];
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    onAuthStateChanged(
+      auth,
+      (user) => {
+        if (user) {
+          const uid = user.uid;
+          setUid(uid);
+        } else {
+        }
+      },
+      [auth]
+    );
+
+    db.collection("notifications")
+      .where("idUser", "==", uid)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          // console.log(doc.id, " => ", doc.data());
+          getNotifications.push(doc.data());
+        });
+        setNotifications(getNotifications);
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
+  }, [uid]);
+
+  const handleClick = (id, index) => {
+    var userRef = db.collection("notifications").doc(id);
+    return userRef
+      .update({
+        isRead: true,
+      })
+      .then(() => {
+        console.log("Document successfully updated!");
+        getNotifications.splice(index, 1);
+        setNotifications(getNotifications);
+      })
+      .catch((error) => {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+      });
+  };
 
   return (
     <Modal open={open}>
       <Box sx={style}>
         <Button onClick={handleClose}>Fermer</Button>
+        <Typography color={theme.palette.text.black}>Notifications</Typography>
+        {notifications.map((notif, index) => {
+          return (
+            <Box>
+              {!notif.isRead && (
+                <Box>
+                  {index}
+                  <Typography color={theme.palette.text.black}>
+                    {notif.content}
+                  </Typography>
+                  <Button
+                    onClick={(e) => {
+                      handleClick(notif.id, index);
+                    }}
+                  >
+                    <Typography>Marquer comme lue</Typography>
+                  </Button>
+                </Box>
+              )}
+            </Box>
+          );
+        })}
       </Box>
     </Modal>
   );
