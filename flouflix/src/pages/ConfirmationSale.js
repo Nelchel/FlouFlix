d2edee82e9061a9f4ea412bc27f5e2f432ec179d
Aurore Wentzel <aurore.wentzel@infomaniak.com>
2022-06-22 14:28:23 +0200

    add date

--- flouflix/src/pages/ConfirmationSale.js
+++ flouflix/src/pages/ConfirmationSale.js
@@ -1,40 +1,36 @@
 import { Typography } from "@mui/material";
-import { makeStyles,useTheme } from "@mui/styles";
+import { makeStyles, useTheme } from "@mui/styles";
 import TextField from "@mui/material/TextField";
 
 import React, { useState, useEffect } from "react";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import {
-  getAuth,
-  onAuthStateChanged,
-} from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import {
   doc,
   updateDoc,
   setDoc,
   where,
   collection,
-  getDocs, 
+  getDocs,
   query,
   arrayRemove,
 } from "firebase/firestore";
-
+import toTimestamp from "../helpers/ToTimestamp";
 
 const ConfirmationSale = (props) => {
-
   const db = firebase.firestore();
   const auth = getAuth();
   const [uid, setUid] = useState("");
   const [moovieInMyCart, setMoovieInMyCart] = useState([]);
-  const [finalPrice,setFinalPrice] = useState(0);
+  const [finalPrice, setFinalPrice] = useState(0);
   const [movieId, setMovieId] = useState("");
-  const [valdiate,setValidate] = useState(false);
+  const [valdiate, setValidate] = useState(false);
   const [quantity, setQuantity] = useState([]);
   const [userCurrent, setUserCurrent] = useState(undefined);
 
-//Récupération des informations de la BDD
+  //Récupération des informations de la BDD
   useEffect(() => {
     onAuthStateChanged(
       auth,
@@ -55,63 +51,70 @@ const ConfirmationSale = (props) => {
         querySnapshot.forEach((doc) => {
           user.push(doc.data());
         });
-        console.log(user[0])
-        if(user[0]!== undefined){
-            const quantityArray = user[0].myCart
-            purchaseHisorty(user[0],quantityArray)
-            suppr(quantityArray);
+        console.log(user[0]);
+        if (user[0] !== undefined) {
+          const quantityArray = user[0].myCart;
+          purchaseHisorty(user[0], quantityArray);
+          suppr(quantityArray);
         }
       })
       .catch((error) => {
         console.log("Error getting documents: ", error);
       });
-
   }, [uid]);
-//Suppréssion du panier
+  //Suppréssion du panier
   const suppr = async (quantityArray) => {
-      for (let index = 0; index < quantityArray.length; index++) {
-          await updateDoc(doc(db, "users", uid),{
-              "myCart":arrayRemove({idMoovie :quantityArray[index].idMoovie,Quantity : quantityArray[index].Quantity})
-          }); 
-      }
-  }
-//Ajout dans la table transaction
-const purchaseHisorty = async(userCurrent,quantityArray) => {
+    for (let index = 0; index < quantityArray.length; index++) {
+      await updateDoc(doc(db, "users", uid), {
+        myCart: arrayRemove({
+          idMoovie: quantityArray[index].idMoovie,
+          Quantity: quantityArray[index].Quantity,
+        }),
+      });
+    }
+  };
+  //Ajout dans la table transaction
+  const purchaseHisorty = async (userCurrent, quantityArray) => {
     let movies = [];
     if (userCurrent !== undefined) {
       const cart = userCurrent.myCart;
       await Promise.all(
         cart.map(async (carte, index) => {
-          const q = query(collection(db, "movies"), where("id", "==", carte.idMoovie));
+          const q = query(
+            collection(db, "movies"),
+            where("id", "==", carte.idMoovie)
+          );
           const querySnapshot = await getDocs(q);
           querySnapshot.forEach((doc) => {
             movies.push(doc.data());
           });
         })
       );
-      console.log(movies)
-      console.log(quantityArray)
-      purchase(movies,quantityArray)
+      console.log(movies);
+      console.log(quantityArray);
+      purchase(movies, quantityArray);
     }
-}
-const purchase = async(movies,quantityArray) => {
-    let infoMovie = []
+  };
+  const purchase = async (movies, quantityArray) => {
+    let infoMovie = [];
+    const currentDate = new Date();
+    const tmp = toTimestamp(currentDate);
     for (let index = 0; index < movies.length; index++) {
-        infoMovie.push({
-            name : movies[index].name,
-            idMoovie :quantityArray[index].idMoovie,
-            quantity :quantityArray[index].Quantity,
-            price : movies[index].price,
-            idPrice : movies[index].id_price,
-        })
-        
+      infoMovie.push({
+        name: movies[index].name,
+        idMoovie: quantityArray[index].idMoovie,
+        quantity: quantityArray[index].Quantity,
+        price: movies[index].price,
+        idPrice: movies[index].id_price,
+      });
     }
     if (uid !== "") {
-        await db
+      await db
         .collection("purchase")
         .add({
-            idUser: uid,
-            purchase : infoMovie,
+          idUser: uid,
+          date: tmp,
+          purchase: infoMovie,
         })
         .then(async (docRef) => {
           const movieRef = await doc(db, "purchase", docRef.id);
@@ -126,16 +129,13 @@ const purchase = async(movies,quantityArray) => {
         });
       window.location.replace(`/historique-achat`);
     }
-}
+  };
 
-
-return(
-        <Typography>
-            Confirmation de votre achat en cours...
-            Veuillez patientez 
-        </Typography>
-    )
-}
-
+  return (
+    <Typography>
+      Confirmation de votre achat en cours... Veuillez patientez
+    </Typography>
+  );
+};
 
 export default ConfirmationSale;
